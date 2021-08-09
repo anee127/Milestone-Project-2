4ab78891a3e3eb0c579c634edce19b18e20c74b9
Aneesa Khan <aneesa.khan97@outlook.com>
2021-08-10 00:21:07 +0100

    custom javascript for flipping the cards

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+  this.classList.toggle('flip');
+  this.classList.add('flip');
+
+   if (!hasFlippedCard) {
+     hasFlippedCard = true;
+     firstCard = this;
+     return;
+   }
+   
+secondCard = this;
+hasFlippedCard = false;
+
+checkForMatch();
+}
+
+function checkForMatch() {
+if (firstCard.dataset.framework === secondCard.dataset.framework) {
+  disableCards();
+  return;
+}
+
+unflipCards();
+}
+
+function disableCards() {
+firstCard.removeEventListener('click', flipCard);
+secondCard.removeEventListener('click', flipCard);
+
+resetBoard();
+}
+
+
+function unflipCards() {
+    lockBoard = true;
+setTimeout(() => {
+  firstCard.classList.remove('flip');
+  secondCard.classList.remove('flip');
+
+  lockBoard = false;
+  resetBoard();
+}, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
+
+(function shuffle() {
+    cards.forEach(card => {
+      let ramdomPos = Math.floor(Math.random() * 12);
+      card.style.order = ramdomPos;
+    });
+  })();  
+
+cards.forEach(card => card.addEventListener('click', flipCard));
